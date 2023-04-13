fa925a658b4da42eb9b018017882369456b78e8a
Kewan Seymour <101571402+Kweezy01@users.noreply.github.com>
2023-04-13 10:03:24 +0200

    Juiced

---
+++ nvim/lua/kweez/init.lua
@@ -0,0 +1,7 @@
+require('kweez.remap')
+require('kweez.set')
+require('kweez.set')
+
+vim.cmd('colorscheme onedark')
+vim.cmd('hi Normal guibg=NONE ctermbg=NONE')
+vim.cmd('hi EndOfBuffer guibg=NONE ctermbg=NONE')
